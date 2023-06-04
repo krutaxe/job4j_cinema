348d318820b1cc8d8c5a0f26a2d46277100c063d
krutaxe <krutaxe@mail.ru>
2023-06-04 18:07:36 +0300

    2. Сервис - Кинотеатр [#504869]

--- db/scripts/001_ddl_create_initial_schema.sql
+++ db/scripts/001_ddl_create_initial_schema.sql
@@ -11,5 +11,43 @@ create table genres
     name varchar unique not null
 );
 
+create table films
+(
+    id                  serial primary key,
+    name                varchar                    not null,
+    description         varchar                    not null,
+    "year"              int                        not null,
+    genre_id            int references genres (id) not null,
+    minimal_age         int                        not null,
+    duration_in_minutes int                        not null,
+    file_id             int references files (id)  not null
+);
+
+create table halls
+(
+    id          serial primary key,
+    name        varchar not null,
+    row_count   int     not null,
+    place_count int     not null,
+    description varchar not null
+);
+
+create table film_sessions
+(
+    id         serial primary key,
+    film_id    int references films (id) not null,
+    halls_id   int references halls (id) not null,
+    start_time timestamp                 not null,
+    end_time   timestamp                 not null,
+    price      int                       not null
+);
+
+create table users
+(
+    id        serial primary key,
+    name varchar        not null,
+    email     varchar unique not null,
+    password  varchar        not null
+);
 
 
