bc6aad8f40c1681e351f3917169844e93da00c32
krutaxe <krutaxe@mail.ru>
2023-06-04 18:06:09 +0300

    2. Сервис - Кинотеатр [#504869]

--- db/scripts/001_ddl_create_initial_schema.sql
+++ db/scripts/001_ddl_create_initial_schema.sql
@@ -1,3 +1,15 @@
+create table files
+(
+    id   serial primary key,
+    name varchar not null,
+    path varchar not null unique
+);
+
+create table genres
+(
+    id   serial primary key,
+    name varchar unique not null
+);
 
 
 
