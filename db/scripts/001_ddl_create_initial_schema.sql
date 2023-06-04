4a56601c3601572b88b6608606d1c4f1cca75b82
krutaxe <krutaxe@mail.ru>
2023-06-04 18:25:19 +0300

    2. Сервис - Кинотеатр [#504869]

--- db/scripts/001_ddl_create_initial_schema.sql
+++ db/scripts/001_ddl_create_initial_schema.sql
@@ -11,17 +11,17 @@ create table genres
     name varchar unique not null
 );
 
-create table films
-(
-    id                  serial primary key,
-    name                varchar                    not null,
-    description         varchar                    not null,
-    age              int                        not null,
-    genre_id            int references genres (id) not null,
-    minimal_age         int                        not null,
-    duration_in_minutes int                        not null,
-    file_id             int references files (id)  not null
-);
+--create table films
+--(
+--    id                  serial primary key,
+--    name                varchar                    not null,
+--    description         varchar                    not null,
+--    age              int                        not null,
+--    genre_id            int references genres (id) not null,
+--    minimal_age         int                        not null,
+--    duration_in_minutes int                        not null,
+--    file_id             int references files (id)  not null
+--);
 
 create table halls
 (
@@ -32,15 +32,15 @@ create table halls
     description varchar not null
 );
 
-create table film_sessions
-(
-    id         serial primary key,
-    film_id    int references films (id) not null,
-    halls_id   int references halls (id) not null,
-    start_time timestamp                 not null,
-    end_time   timestamp                 not null,
-    price      int                       not null
-);
+--create table film_sessions
+--(
+--    id         serial primary key,
+--    film_id    int references films (id) not null,
+--    halls_id   int references halls (id) not null,
+--    start_time timestamp                 not null,
+--    end_time   timestamp                 not null,
+--    price      int                       not null
+--);
 
 create table users
 (
@@ -50,15 +50,15 @@ create table users
     password  varchar        not null
 );
 
-create table tickets
-(
-    id           serial primary key,
-    session_id   int references film_sessions (id) not null,
-    row_number   int                               not null,
-    place_number int                               not null,
-    user_id      int                               not null,
-    unique (session_id, row_number, place_number)
-);
+--create table tickets
+--(
+--    id           serial primary key,
+--    session_id   int references film_sessions (id) not null,
+--    row_number   int                               not null,
+--    place_number int                               not null,
+--    user_id      int                               not null,
+--    unique (session_id, row_number, place_number)
+--);
 
 
 
